2e35bf25ad97d7bedf81fc3d6bca57983d3b7aa9
jlzikong <zikong@appest.com>
2013-07-08 16:41:56 +0800

    create project

---
+++ TickTickHD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TickTickHD
+//
+//  Created by zikong on 13-7-8.
+//  Copyright (c) 2013年 com.TickTIck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
